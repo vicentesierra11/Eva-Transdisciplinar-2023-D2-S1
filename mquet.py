0b7bea7018c80e6d8ef1ab183b8cf74e02f75701
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2023-06-01 16:20:22 -0400

    display ventana
    se incorporo las dimensiones del display de  la ventana

---
+++ mquet.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import pygame
+
+def fondo():
+    canvas.create_image(0, 0, anchor="center", image="???")
+
+ventana = tk.Tk()
+ventana.geometry("1200x800")
+ventana.title("Movimiento Rectilineo Uniforme")
+
+screen_width = ventana.winfo_screenwidth()
+screen_height = ventana.winfo_screenheight()
+
+canvas = tk.Canvas(ventana, width=screen_width, height=screen_height)
+canvas.pack()
