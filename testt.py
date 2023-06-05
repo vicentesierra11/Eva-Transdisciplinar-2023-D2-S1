285944b2d1a5af09623c0fd74f3f77a6bddf8a75
vicentesierra11 <vsierra2023@alu.uct.cl>
2023-06-05 14:29:04 -0400

    Creación de la ventana main y fondo
    En este código se inserta la ventana y la imagen de fondo definiendo los espacios para las nuevas funciones.
    Me atrase en la entrega por temas de desconocimiento de git y un pequeño problema de salud.

---
+++ testt.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from PIL import ImageTk, Image
+
+# Crear la ventana principal con resolución
+ventana = tk.Tk()
+ventana.geometry('1200x700')
+ventana.resizable(False, False)
+
+# Cargar las imágenes
+imagen_fondo = ImageTk.PhotoImage(Image.open("marco.png"))
+imagen_fondo1 = Image.open("marco.png")
+imagen_fondo1 = imagen_fondo1.resize((1200, 600), Image.ANTIALIAS)
+imagen_superpuesta = Image.open("bkg2.jpg")
+imagen_superpuesta = imagen_superpuesta.resize((659, 339), Image.ANTIALIAS)
+imagen_superpuesta = ImageTk.PhotoImage(imagen_superpuesta)
+
+# Crear un widget Label con la imagen de fondo
+label_fondo = tk.Label(ventana, image=imagen_fondo)
+label_fondo.place(x=0, y=0)
+
+# Crear un widget Label con la imagen superpuesta
+label_superpuesta = tk.Label(ventana, image=imagen_superpuesta, borderwidth=0)
+label_superpuesta.place(x=32, y=40)
+
+# Ejecutar el bucle principal de la interfaz
+ventana.mainloop()
