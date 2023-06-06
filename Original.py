ba520bf0dfc316e0bd7ff5aa5e111b7020d40777
Carlos Mora <cmora2023@alu.uct.cl>
2023-06-06 11:41:20 -0400

    Se reorganiza las funciones por boton, interfaces y operatorias
    se separa las funciones operatorias matemática de las interfaces, dejándolas por separadas, dando una mejor organización y comentada para que el resto del equipo entienda lo realizado

--- Original.py
+++ Original.py
@@ -62,131 +62,185 @@ for formula in formulas:
 
 ###############################################################################
 
-def cal_velocidad():       
-    def calcular_resultado():
-        xi = int(entry_xi.get())
-        xf = int(entry_xf.get())
-        ti = int(entry_ti.get())
-        tf = int(entry_tf.get())
-        resultado = round((xi - xf) / (ti - tf),2)
-        label_resultado.config(text="Resultado: " + str(resultado))
+# Botones seleccion operatoria
+boton1 = tk.Button(ventana, text="Velocidad", command=lambda: [interfaz_velocidad()], width=16, height=3)
+boton1.pack()
+boton1.place(x=154, y=411)
 
-    label_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
-    label_xi.place(x=169, y=500)
-    entry_xi = tk.Entry(ventana)
-    entry_xi.place(x=191, y=525, width=35)
-
-    operador = tk.Label(ventana, text=" - ", fg="red")
-    operador.place(x=305, y=511)
-
-    label_xf = tk.Label(ventana, text="Posición final: ", fg="green")
-    label_xf.place(x=357, y=500)
-    entry_xf = tk.Entry(ventana)
-    entry_xf.place(x=377, y=525, width=35)
-
-    label_ti = tk.Label(ventana, text="Tiempo inicial: ", fg="green")
-    label_ti.place(x=169, y=550)
-    entry_ti = tk.Entry(ventana)
-    entry_ti.place(x=191, y=574, width=35)
-
-    operador = tk.Label(ventana, text=" - ", fg="red")
-    operador.place(x=305, y=565)
-
-    label_tf = tk.Label(ventana, text="Tiempo final:", fg="green")
-    label_tf.place(x=357, y=550)
-    entry_tf = tk.Entry(ventana)
-    entry_tf.place(x=377, y=574, width=35)
-
-    botoncal = tk.Button(ventana, text="Calcular", fg="blue", command=calcular_resultado)
-    botoncal.place(x=490, y=510)
-
-    label_resultado = tk.Label(ventana, text="-")
-    label_resultado.place(x=490, y=549)
-
-
-def cal_posicion():
-    def calcular_resultado():
-        xi = int(entry_xi.get())
-        v = int(entry_vi.get())
-        t = int(entry_ti.get())
-        resultado = (v * t) + xi
-        label_resultado.config(text="resultado: " + str(resultado))
-
-    label_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
-    label_xi.place(x=168, y=500)
-    entry_xi = tk.Entry(ventana)
-    entry_xi.place(x=191, y=530, width=35)
-
-    operador = tk.Label(ventana, text=" + ", fg="red")
-    operador.place(x=277, y=522)
-
-    label_vi = tk.Label(ventana, text="Velocidad: ", fg="green")
-    label_vi.place(x=311, y=500)
-    entry_vi = tk.Entry(ventana)
-    entry_vi.place(x=325, y=530, width=35)
-
-    operador = tk.Label(ventana, text=" * ", fg="red")
-    operador.place(x=405, y=522)
-
-    label_ti = tk.Label(ventana, text="Tiempo : ", fg="green")
-    label_ti.place(x=440, y=500)
-    entry_ti = tk.Entry(ventana)
-    entry_ti.place(x=454, y=530, width=35)
-
-    botoncal = tk.Button(ventana, text="calcular", fg="blue", command=calcular_resultado)
-    botoncal.place(x=400, y=570)
-
-    label_resultado = tk.Label(ventana, text="-")
-    label_resultado.place(x=475, y=590)
-
-
-def cal_tiempo():
-    def calcular_resultado():
-        xi = int(entry_xi.get())
-        xf = int(entry_xf.get())
-        v = int(entry_v.get())
-        resultado = (xi - xf) / v
-        label_resultado.config(text="resultado: " + str(resultado))
-
-    label_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
-    label_xi.place(x=168, y=500)
-    entry_xi = tk.Entry(ventana)
-    entry_xi.place(x=191, y=530, width=35)
-
-    operador = tk.Label(ventana, text=" - ", fg="red")
-    operador.place(x=277, y=522)
-
-    label_xf = tk.Label(ventana, text="Posición final: ", fg="green")
-    label_xf.place(x=311, y=500)
-    entry_xf = tk.Entry(ventana)
-    entry_xf.place(x=325, y=530, width=35)
-
-    operador = tk.Label(ventana, text=" / ", fg="red")
-    operador.place(x=405, y=522)
-
-    label_v = tk.Label(ventana, text="Velocidad: ", fg="green")
-    label_v.place(x=440, y=500)
-    entry_v = tk.Entry(ventana)
-    entry_v.place(x=454, y=530, width=35)
-
-    botoncal = tk.Button(ventana, text="calcular", fg="blue", command=calcular_resultado)
-    botoncal.place(x=400, y=570)
-
-    label_resultado = tk.Label(ventana, text="-")
-    label_resultado.place(x=475, y=590)
-
-
-boton = tk.Button(ventana, text="Velocidad", command= cal_velocidad, width=16, height=3)
-boton.pack()
-boton.place(x=154, y=411)
-
-boton2 = tk.Button(ventana, text="Posición", command= cal_posicion, width=16, height=3)
+boton2 = tk.Button(ventana, text="Posición", command=lambda: [interfaz_posicion()], width=16, height=3)
 boton2.pack()
 boton2.place(x=282, y=411)
 
-boton3 = tk.Button(ventana, text="Tiempo", command= cal_tiempo, width=16, height=3)
+boton3 = tk.Button(ventana, text="Tiempo", command=lambda: [interfaz_tiempo()], width=16, height=3)
 boton3.pack()
 boton3.place(x=410, y=411)
+# interfaces
+def interfaz_velocidad():
+    global label_v_xi, label_v_operador, label_v_xf, label_v_ti, label_v_operador2, label_v_tf, label_v_resultado
+    global entry_v_xi, entry_v_xf, entry_v_ti, entry_v_tf, boton_v_calcular
+    # //Label (etiquetas)://
+        #----1° parte operacion---
+    label_v_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
+    label_v_operador = tk.Label(ventana, text=" - ", fg="red")
+    label_v_xf = tk.Label(ventana, text="Posición final: ", fg="green")
+        #----2° parte operacion---
+    label_v_ti = tk.Label(ventana, text="Tiempo inicial: ", fg="green")
+    label_v_operador2 = tk.Label(ventana, text=" - ", fg="red")
+    label_v_tf = tk.Label(ventana, text="Tiempo final:", fg="green")
+        #----resultado-----------
+    label_v_resultado = tk.Label(ventana, text="-")
+
+    # //Entry (campos de entrada):// 
+        #----1° parte operacion---
+    entry_v_xi = tk.Entry(ventana)
+    entry_v_xf = tk.Entry(ventana)
+        #----2° parte operacion---
+    entry_v_ti = tk.Entry(ventana)
+    entry_v_tf = tk.Entry(ventana)
+
+    # //Boton //
+    boton_v_calcular = tk.Button(ventana, text="Calcular", fg="blue", command=operatoria_velocidad)
+
+    # //Places (cordenadas: (x,y))://
+        # /Places (etiquetas)/
+        #---- 1° parte operacion----
+    label_v_xi.place(x=169, y=500)
+    label_v_operador.place(x=305, y=511)
+    label_v_xf.place(x=357, y=500)
+        #-----2° parte operacion---
+    label_v_ti.place(x=169, y=550)
+    label_v_operador2.place(x=305, y=565)
+    label_v_tf.place(x=357, y=550)
+        #-------reultado-----------
+    label_v_resultado.place(x=490, y=549)
+
+        # /Places (campos de entradas)/
+    entry_v_xi.place(x=191, y=525, width=35)
+    entry_v_xf.place(x=377, y=525, width=35)
+        #------------------------
+    entry_v_ti.place(x=191, y=574, width=35)
+    entry_v_tf.place(x=377, y=574, width=35)
+
+        # /Place (boton)/
+    boton_v_calcular.place(x=490, y=510)
+
+def interfaz_tiempo():
+    global label_t_xi, label_t_operador, label_t_xf, label_t_operador2, label_t_v, label_t_resultado
+    global entry_t_xi, entry_t_xf, entry_t_v, boton_t_calcular
+    # //Label (etiquetas)://
+        #----1° parte operatoria---
+    label_t_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
+    label_t_operador = tk.Label(ventana, text=" - ", fg="red")
+    label_t_xf = tk.Label(ventana, text="Posición final: ", fg="green")
+        #----2° parte operatoria----
+    label_t_operador2 = tk.Label(ventana, text=" / ", fg="red")
+    label_t_v = tk.Label(ventana, text="Velocidad: ", fg="green")
+        #-------resultado--- 
+    label_t_resultado = tk.Label(ventana, text="-")
+    
+    # // Entry (campos de entrada)//
+        #---- 1° parte operatoria---
+    entry_t_xi = tk.Entry(ventana)
+    entry_t_xf = tk.Entry(ventana)
+        #-----2° parte operatoria---
+    entry_t_v = tk.Entry(ventana)
+    
+    # //Boton//
+    boton_t_calcular = tk.Button(ventana, text="calcular", fg="blue", command=operatoria_tiempo)
+    
+    # //Places (coordenadas: (x,y))//
+        # /places (etiquetas)/
+        #---1° parte operatoria----
+    label_t_xi.place(x=168, y=500)
+    label_t_operador.place(x=277, y=522)
+    label_t_xf.place(x=311, y=500)
+        #---2° parte operatoria---
+    label_t_operador2.place(x=405, y=522)
+    label_t_v.place(x=440, y=500)
+        #------resultado-----
+    label_t_resultado.place(x=475, y=590)
+    
+        # /places (campos de entrada)/
+        #---- 1° parte operatoria----
+    entry_t_xi.place(x=191, y=530, width=35)
+    entry_t_xf.place(x=325, y=530, width=35)
+        #-----2° parte operatoria----
+    entry_t_v.place(x=454, y=530, width=35)
+    
+        # /place (boton)/
+    boton_t_calcular.place(x=400, y=570)
+
+def interfaz_posicion():
+    global label_p_xi, label_p_operador, label_p_vi, label_p_operador2, label_p_ti, label_p_resultado
+    global entry_p_xi, entry_p_vi, entry_p_ti, boton_p_calcular
+    
+    # //Label (etiquetas)//
+        #----1° parte operatoria---
+    label_p_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
+    label_p_operador = tk.Label(ventana, text=" + ", fg="red")
+    label_p_vi = tk.Label(ventana, text="Velocidad: ", fg="green")
+        #----2° parte operatoria---
+    label_p_operador2 = tk.Label(ventana, text=" * ", fg="red")
+    label_p_ti = tk.Label(ventana, text="Tiempo : ", fg="green")
+        #-----resultado-----
+    label_p_resultado = tk.Label(ventana, text="-")
+    
+    # //Entry (campos de entrada)///
+        #---1° parte operatoria---
+    entry_p_xi = tk.Entry(ventana)
+    entry_p_vi = tk.Entry(ventana)
+        #---2° parte operatoria---
+    entry_p_ti = tk.Entry(ventana)
+    
+    # //Boton//
+    boton_p_calcular = tk.Button(ventana, text="calcular", fg="blue", command=operatoria_posicion)
+    
+    # //Places (cordenadas(x,y))//
+        # /places (etiquetas)/
+        #---1° parte operatoria---
+    label_p_xi.place(x=168, y=500)
+    label_p_operador.place(x=277, y=522)
+    label_p_vi.place(x=311, y=500)
+        #---2° parte operatoria---
+    label_p_operador2.place(x=405, y=522)
+    label_p_ti.place(x=440, y=500)
+        #------resultado-------
+    label_p_resultado.place(x=475, y=590)
+    
+        # /places (campos de entrada)/
+        #---1° parte operatoria----
+    entry_p_xi.place(x=191, y=530, width=35)
+    entry_p_vi.place(x=325, y=530, width=35)
+        #---2° parte operatoria----
+    entry_p_ti.place(x=454, y=530, width=35)
+    
+        # /places (boton)/
+    boton_p_calcular.place(x=400, y=570)
+
+# operatorias matematicas
+def operatoria_velocidad():
+    v_xi = int(entry_v_xi.get())
+    v_xf = int(entry_v_xf.get())
+    #-------------------------------
+    v_ti = int(entry_v_ti.get())
+    v_tf = int(entry_v_tf.get())
+    #-------------------------------
+    resultado_v = round((v_xi - v_xf) / (v_ti - v_tf),2)
+    label_v_resultado.config(text="Resultado: " + str(resultado_v))
+
+def operatoria_tiempo():
+    t_xi = int(entry_t_xi.get())
+    t_xf = int(entry_t_xf.get())
+    t_v = int(entry_t_v.get())
+    resultado_t = (t_xi - t_xf) / t_v
+    label_t_resultado.config(text="resultado: " + str(resultado_t))
+
+def operatoria_posicion():
+    p_xi = int(entry_p_xi.get())
+    p_v = int(entry_p_vi.get())
+    p_t = int(entry_p_ti.get())
+    resultado_p = (p_v * p_t) + p_xi
+    label_p_resultado.config(text="resultado: " + str(resultado_p))
 
 ###############################################################################
 ###############################################################################
