5257206dafd54600dfece776f5805fda677250b0
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2023-06-27 10:36:33 -0400

    implementación grafica y animación para velocidad
    implementación de la grafica con la variable del resultado de velocidad al ser calculada por la formula de velocidad

--- mquet.py
+++ mquet.py
@@ -1,14 +1,20 @@
-import tkinter as tk, turtle
+import tkinter as tk
+from tkinter import *   
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from PIL import ImageTk, Image
-
+import turtle
 # Crear la ventana principal con resolución
 ventana = tk.Tk()
 ventana.geometry('1200x650')
 ventana.resizable(False, False)
 ventana.title("M.U.R - Movimiento Uniforme rectilineo")
+
+# ESCONDER
+show_velocidad = True
+numeroesconder = -1
+
 
 # Cargar las imágenes
 imagen_fondo = ImageTk.PhotoImage(Image.open("marco.png"))
@@ -30,10 +36,9 @@ label_superpuesta.place(x=32, y=40)
 fig, ax = plt.subplots(figsize=(4.38, 3.53))
 x = np.array([1, 2, 3, 4])
 y = np.array([1, 2, 3, 4])
-ax.plot(x, y)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_title('M.U.R')
+ax.set_xlabel('Tiempo')
+ax.set_ylabel('Posición')
+ax.set_title('Relación entre Posición y Tiempo')
 ax.grid(True)
 
 # Crear el widget de lienzo de la gráfica
@@ -63,76 +68,116 @@ for formula in formulas:
 
 ###############################################################################
 
-# Botones seleccion operatoria
-boton1 = tk.Button(ventana, text="Velocidad", command=lambda: [interfaz_velocidad()], width=16, height=3)
-boton1.pack()
-boton1.place(x=154, y=411)
+#------------------------------------------------------------------------------
+#---------------------------- Interfaz Velocidad ------------------------------
+#------------------------------------------------------------------------------
 
-boton2 = tk.Button(ventana, text="Posición", command=lambda: [interfaz_posicion()], width=16, height=3)
-boton2.pack()
-boton2.place(x=282, y=411)
-
-boton3 = tk.Button(ventana, text="Tiempo", command=lambda: [interfaz_tiempo()], width=16, height=3)
-boton3.pack()
-boton3.place(x=410, y=411)
-
-# interfaces
 def interfaz_velocidad():
     global label_v_xi, label_v_operador, label_v_xf, label_v_ti, label_v_operador2, label_v_tf, label_v_resultado
-    global entry_v_xi, entry_v_xf, entry_v_ti, entry_v_tf, boton_v_calcular
+    global entry_v_xi, entry_v_xf, entry_v_ti, entry_v_tf, boton_v_calcular, numeroesconder
+
+    def esconder_velocidad():
+        label_v_xi.place_forget()
+        label_v_operador.place_forget()
+        label_v_xf.place_forget()
+        label_v_ti.place_forget()
+        label_v_operador2.place_forget()
+        label_v_tf.place_forget()
+        label_v_resultado.place_forget()
+        entry_v_xi.place_forget()
+        entry_v_xf.place_forget()
+        entry_v_ti.place_forget()
+        entry_v_tf.place_forget()
+        boton_v_calcular.place_forget()
+        boton_limpiar.place_forget()
+        ventana.update()
+
     # //Label (etiquetas)://
         #----1° parte operacion---
-    label_v_xi = tk.Label(ventana, text="Posición final: ", fg="green")
+
+    boton_limpiar = tk.Button(ventana, text="Esconder", fg="blue", command=esconder_velocidad)
+    boton_limpiar.place(x=180 , y= 600)
+
+    numeroesconder = -1 * numeroesconder
+    
+    label_v_xi = tk.Label(ventana, text="Posición Final: ", fg="green")
     label_v_operador = tk.Label(ventana, text=" - ", fg="red")
-    label_v_xf = tk.Label(ventana, text="Posición inicial: ", fg="green")
-        #----2° parte operacion---
-    label_v_ti = tk.Label(ventana, text="Tiempo final: ", fg="green")
+    label_v_xf = tk.Label(ventana, text="Posición Inicial: ", fg="green")
+            #----2° parte operacion---
+    label_v_ti = tk.Label(ventana, text="Tiempo Final: ", fg="green")
     label_v_operador2 = tk.Label(ventana, text=" - ", fg="red")
-    label_v_tf = tk.Label(ventana, text="Tiempo inicial:", fg="green")
+    label_v_tf = tk.Label(ventana, text="Tiempo Inicial:", fg="green")
         #----resultado-----------
     label_v_resultado = tk.Label(ventana, text="-")
 
-    # //Entry (campos de entrada):// 
-        #----1° parte operacion---
+        # //Entry (campos de entrada):// 
+            #----1° parte operacion---
     entry_v_xi = tk.Entry(ventana)
     entry_v_xf = tk.Entry(ventana)
-        #----2° parte operacion---
+            #----2° parte operacion---
     entry_v_ti = tk.Entry(ventana)
     entry_v_tf = tk.Entry(ventana)
 
-    # //Boton //
+        # //Boton //
     boton_v_calcular = tk.Button(ventana, text="Calcular", fg="blue", command=operatoria_velocidad)
 
-    # //Places (cordenadas: (x,y))://
-        # /Places (etiquetas)/
-        #---- 1° parte operacion----
+        # //Places (cordenadas: (x,y))://
+            # /Places (etiquetas)/
+            #---- 1° parte operacion----
     label_v_xi.place(x=169, y=500)
     label_v_operador.place(x=305, y=511)
     label_v_xf.place(x=357, y=500)
-        #-----2° parte operacion---
+            #-----2° parte operacion---
     label_v_ti.place(x=169, y=550)
     label_v_operador2.place(x=305, y=565)
     label_v_tf.place(x=357, y=550)
-        #-------reultado-----------
+            #-------reultado-----------
     label_v_resultado.place(x=490, y=549)
 
-        # /Places (campos de entradas)/
+            # /Places (campos de entradas)/
     entry_v_xi.place(x=191, y=525, width=35)
     entry_v_xf.place(x=377, y=525, width=35)
-        #------------------------
+            #------------------------
     entry_v_ti.place(x=191, y=574, width=35)
     entry_v_tf.place(x=377, y=574, width=35)
 
-        # /Place (boton)/
+            # /Place (boton)/
+
     boton_v_calcular.place(x=490, y=510)
+
+    
+    #if numeroesconder == -1:
+    #    esconder_velocidad()
+
+#------------------------------------------------------------------------------
+#---------------------------- Interfaz Tiempo ---------------------------------
+#------------------------------------------------------------------------------
 
 def interfaz_tiempo():
     global label_t_xi, label_t_operador, label_t_xf, label_t_operador2, label_t_v, label_t_resultado
     global entry_t_xi, entry_t_xf, entry_t_v, boton_t_calcular
 
-    entry_v_xi.pack_forget()
+    def esconder_tiempo():
+        label_t_xi.place_forget()
+        label_t_operador.place_forget()
+        label_t_xf.place_forget()
+        label_t_operador2.place_forget()
+        label_t_v.place_forget()
+        label_t_resultado.place_forget()
+        entry_t_xi.place_forget()
+        entry_t_xf.place_forget()
+        entry_t_v.place_forget()
+        boton_t_calcular.place_forget()
+        boton_limpiarT.place_forget()
+        ventana.update()
+
+    boton_limpiarT = tk.Button(ventana, text="Esconder", fg="blue", command=esconder_tiempo)
+    boton_limpiarT.place(x=400, y=600)
+
+#    entry_v_xi.pack_forget() -- FUNCION PARA OCULTAR
     # //Label (etiquetas)://
         #----1° parte operatoria---
+
     label_t_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
     label_t_operador = tk.Label(ventana, text=" - ", fg="red")
     label_t_xf = tk.Label(ventana, text="Posición final: ", fg="green")
@@ -174,10 +219,30 @@ def interfaz_tiempo():
         # /place (boton)/
     boton_t_calcular.place(x=400, y=570)
 
+#------------------------------------------------------------------------------
+#---------------------------- Interfaz Tiempo ---------------------------------
+#------------------------------------------------------------------------------
+
 def interfaz_posicion():
     global label_p_xi, label_p_operador, label_p_vi, label_p_operador2, label_p_ti, label_p_resultado
     global entry_p_xi, entry_p_vi, entry_p_ti, boton_p_calcular
     
+    def esconder_posicion():
+        label_p_xi.place_forget()
+        label_p_operador.place_forget()
+        label_p_vi.place_forget()
+        label_p_operador2.place_forget()
+        label_p_ti.place_forget()
+        label_p_resultado.place_forget()
+        entry_p_xi.place_forget()
+        entry_p_vi.place_forget()
+        entry_p_ti.place_forget()
+        boton_p_calcular.place_forget()
+        boton_esconderP.place_forget()
+        ventana.update()
+
+    boton_esconderP = tk.Button(ventana, text="Esconder", fg="blue", command=esconder_posicion)
+    boton_esconderP.place(x=250,y=600)
     # //Label (etiquetas)//
         #----1° parte operatoria---
     label_p_xi = tk.Label(ventana, text="Posición inicial: ", fg="green")
@@ -195,7 +260,7 @@ def interfaz_posicion():
     entry_p_vi = tk.Entry(ventana)
         #---2° parte operatoria---
     entry_p_ti = tk.Entry(ventana)
-    
+
     # //Boton//
     boton_p_calcular = tk.Button(ventana, text="calcular", fg="blue", command=operatoria_posicion)
     
@@ -221,8 +286,13 @@ def interfaz_posicion():
         # /places (boton)/
     boton_p_calcular.place(x=400, y=570)
 
-# operatorias matematicas
+
+#----------------------------------------------------------------------------
+#--------------------- Operatorias ------------------------------------------
+#----------------------------------------------------------------------------
+
 def operatoria_velocidad():
+    global resultado_v
     v_xi = int(entry_v_xi.get())
     v_xf = int(entry_v_xf.get())
     #-------------------------------
@@ -246,21 +316,79 @@ def operatoria_posicion():
     resultado_p = (p_v * p_t) + p_xi
     label_p_resultado.config(text="resultado: " + str(resultado_p))
 
-###############################################################################
-###############################################################################
-###############################################################################
+#----------------------------------------------------------------------------------------------
+#------------------------------- Botones seleccion operatoria ---------------------------------
+#----------------------------------------------------------------------------------------------
 
-def animacion_flecha_MUR():
-    # Función para mover la flecha en MUR
-    def mover_flecha():
+
+
+
+# Cargar y redimensionar la imagen
+imagen1 = Image.open("boton1.png")
+imagen1 = imagen1.resize((123, 53), Image.LANCZOS)
+imagen1 = ImageTk.PhotoImage(imagen1)
+
+# Crear el botón con la imagen
+boton1 = tk.Button(ventana, image=imagen1, command=lambda: [interfaz_velocidad()])
+
+# Asignar la coordenada al botón
+boton1.place(x=154, y=409)
+
+#####################################
+imagen2 = Image.open("boton2.png")
+imagen2 = imagen2.resize((123, 53), Image.LANCZOS)
+imagen2 = ImageTk.PhotoImage(imagen2)
+
+# Crear el botón con la imagen
+boton2 = tk.Button(ventana, image=imagen2, command=lambda: [interfaz_posicion()])
+boton2.place(x=282, y=409)
+#####################################################################
+# Asignar la coordenada al botón
+imagen3 = Image.open("boton3.png")
+imagen3 = imagen3.resize((123, 53), Image.LANCZOS)
+imagen3 = ImageTk.PhotoImage(imagen3)
+
+# Crear el botón con la imagen
+boton3 = tk.Button(ventana, image=imagen3, command=lambda: [interfaz_tiempo()])
+boton3.place(x=410, y=409)
+
+def trazar_grafica():
+    v_xi = int(entry_v_xi.get())
+    v_xf = int(entry_v_xf.get())
+    v_ti = int(entry_v_ti.get())
+    v_tf = int(entry_v_tf.get())
+
+    posiciones = [v_xi, v_xf]
+    tiempos = [v_ti, v_tf]
+
+    ax.clear()
+    ax.plot(tiempos, posiciones, marker='o')
+    ax.set_xlabel('Tiempo')
+    ax.set_ylabel('Posición')
+    ax.set_title('Relación entre Posición y Tiempo')
+    ax.grid(True)
+
+    lienzo_grafica.draw()
+
+################################################################################
+##########Animacion De Pelota##################################################
+################################################################################
+def animacion_MUR():
+    # Función para mover la pelota en MUR
+    def mover_pelota():
         # Crear el lienzo de dibujo con la imagen superpuesta
-        lienzo = tk.Canvas(ventana, width=658, height=339, highlightthickness=0)
+        lienzo = tk.Canvas(ventana, width=659, height=339, highlightthickness=0)
         lienzo.place(x=32, y=40)
 
         # Crear el objeto Turtle para la flecha
-        flecha = turtle.RawTurtle(lienzo)
-        flecha.shape("arrow")
-        flecha.color("blue")
+        pelota = turtle.RawTurtle(lienzo)
+        pelota.hideturtle()
+        pelota.penup()
+        pelota.shape("circle")
+        pelota.color("red")
+        pelota.setposition(-336, 0)
+        pelota.penup()
+        pelota.showturtle()
 
         # Definir las coordenadas iniciales de la flecha
         x_inicial = -335  # Coordenada x del borde izquierdo del lienzo
@@ -269,31 +397,43 @@ def animacion_flecha_MUR():
         # Definir los límites de movimiento de la flecha
         limite_izquierdo = -335
         limite_derecho = 324
-        # Definir la velocidad de la flecha
-        velocidad = 3
 
-        # Mover la flecha en línea recta
+        # Definir la velocidad de la flecha
+        velocidad = resultado_v
         while True:
             # Mover la flecha en línea recta
             x_inicial += velocidad
-            flecha.goto(x_inicial, y_inicial)
-
-            # Verificar si la flecha ha alcanzado los límites de movimiento
-            if x_inicial >= limite_derecho or x_inicial <= limite_izquierdo:
-                velocidad *= -1  # Invertir la dirección de movimiento
+            pelota.goto(x_inicial, y_inicial)
 
             # Actualizar el lienzo para mostrar los cambios
             lienzo.update()
 
-    mover_flecha()
+            # Verificar si la pelota ha alcanzado el límite derecho
+            if x_inicial >= limite_derecho:
+                pelota.hideturtle()  # Ocultar la pelota
+                x_inicial = limite_izquierdo  # Reiniciar la posición de la flecha
+                pelota.goto(x_inicial, y_inicial)  # Mover la flecha a la nueva posición sin dibujar
+                pelota.showturtle()  # Mostrar la pelota nuevamente
 
+    mover_pelota()
 
-def hacer_clic():
-    animacion_flecha_MUR()
+def hacer_click():
+    animacion_MUR()
 
-boton = tk.Button(ventana, text="Iniciar", command=hacer_clic)
-boton.pack()
-boton.place(x=315, y=470)
+def botonesxd():
+    trazar_grafica()
+    hacer_click()
+##################################################################################################################################
+##################################################################################################################################
+##################################################################################################################################
 
-# Ejecutar el bucle principal de la interfaz
+"""# Asociar la función hacer_clic al evento de clic del botón
+boton_animacionv = tk.Button(ventana, text="Iniciar animación", command=hacer_clic)
+boton_animacionv.pack()"""
+
+# Crear el botón para trazar la gráfica
+boton_trazar = tk.Button(ventana, text="Trazar Gráfica", command=botonesxd)
+boton_trazar.place(x=600, y=550)
+
+#--- Bucle Ventana ---#
 ventana.mainloop()
